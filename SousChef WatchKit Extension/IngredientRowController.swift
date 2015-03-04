bc7ddb04105593cd538d2e2d586a12f16fa5efef
butb0rn <xxesnxx@gmail.com>
2015-03-04 11:23:39 -0500

    Add a table of ingredients for the recipe you selected

---
+++ SousChef WatchKit Extension/IngredientRowController.swift
@@ -0,0 +1,16 @@
+//
+//  IngredientRowController.swift
+//  SousChef
+//
+//  Created by Ehsan Valizadeh on 3/4/15.
+//  Copyright (c) 2015 Ray Wenderlich. All rights reserved.
+//
+
+import UIKit
+import WatchKit
+
+class IngredientRowController: NSObject {
+  
+  @IBOutlet weak var measurementLabel: WKInterfaceLabel!
+  @IBOutlet weak var nameLabel: WKInterfaceLabel!
+}
---
+++ SousChef WatchKit Extension/StepRowController.swift
@@ -0,0 +1,16 @@
+//
+//  StepRowController.swift
+//  SousChef
+//
+//  Created by Ehsan Valizadeh on 3/4/15.
+//  Copyright (c) 2015 Ray Wenderlich. All rights reserved.
+//
+
+import UIKit
+import WatchKit
+
+class StepRowController: NSObject {
+  @IBOutlet weak var stepLabel: WKInterfaceLabel!
+   
+  @IBOutlet weak var directionsLabel: WKInterfaceLabel!
+}
